02c73007c31339f8eff3396b40347d535eae4561
Paweł Marks <pmarks@virtuslab.com>
2023-09-06 14:51:40 +0200

    Add transparent inline excercise

---
+++ .repository/04-02/empty.scala
@@ -0,0 +1,18 @@
+package e04
+
+// Excercise 04: Transparent inlines
+// Uncomment the commented lines in the run method and try to make it compile by changing the signature of `empty`.
+// Then, modify the `empty` method in such a way that it fails during the compilation if the name of the type is not recognized, insted of crashing at runtime.
+
+def run =
+  val intList: List[Int] = 42 :: empty("List")
+  val stringList: List[String] = empty("String") :: empty("List")
+  val number: Int = empty("Int") * 2
+  println("ok?")
+
+transparent inline def empty(name: String) =
+  inline name match
+    case "List" => List.empty
+    case "String" => ""
+    case "Int" => 0
+    case _: String => throw IllegalArgumentException()
